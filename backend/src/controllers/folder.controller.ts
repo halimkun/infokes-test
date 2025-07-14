1298963168cd4c9d46dfdf91a3cd22e396136b33
Faisal Halim <ffaisalhalim@gmail.com>
2025-07-15 05:21:10 +0700

    :sparkles: feat: enhance folder controller with error handling for ID parameters

--- backend/src/controllers/folder.controller.ts
+++ backend/src/controllers/folder.controller.ts
@@ -2,17 +2,73 @@ import { FolderService } from '../services/folder.service'
 
 export const FolderController = {
     getAll: async () => FolderService.getAllFolders(),
-    getOne: async ({ params }: any) => FolderService.getFolder(Number(params.id)),
+
+    getOne: async ({ params }: any) => {
+        if (!params.id) {
+            throw new Error('Folder ID is required')
+        }
+
+        if (isNaN(Number(params.id))) {
+            throw new Error('Folder ID must be a number')
+        }
+
+        return FolderService.getFolder(Number(params.id))
+    },
+
     create: async ({ body }: any) => {
         const { name, parentId } = body
         return FolderService.createFolder(name, parentId)
     },
+
     update: async ({ params, body }: any) => {
         const { name, parentId } = body
+
+        if (!params.id) {
+            throw new Error('Folder ID is required')
+        }
+
+        if (isNaN(Number(params.id)) || (parentId && isNaN(Number(parentId)))) {
+            throw new Error('Folder ID must be a number')
+        }
+
         return FolderService.updateFolder(Number(params.id), name, parentId)
     },
-    delete: async ({ params }: any) => FolderService.deleteFolder(Number(params.id)),
-    deleteRecursive: async ({ params }: any) => FolderService.deleteRecursiveFolder(Number(params.id)),
-    getByParentId: async ({ params }: any) => FolderService.getFoldersByParentId(Number(params.parentId)),
+
+    delete: async ({ params }: any) => {
+        if (!params.id) {
+            throw new Error('Folder ID is required')
+        }
+
+        if (isNaN(Number(params.id))) {
+            throw new Error('Folder ID must be a number')
+        }
+
+        return FolderService.deleteFolder(Number(params.id))
+    },
+
+    deleteRecursive: async ({ params }: any) => {
+        if (!params.id) {
+            throw new Error('Folder ID is required')
+        }
+
+        if (isNaN(Number(params.id))) {
+            throw new Error('Folder ID must be a number')
+        }
+
+        return FolderService.deleteRecursiveFolder(Number(params.id))
+    },
+
+    getByParentId: async ({ params }: any) => {
+        if (!params.parentId) {
+            throw new Error('Parent ID is required')
+        }
+
+        if (isNaN(Number(params.parentId))) {
+            throw new Error('Parent ID must be a number')
+        }
+
+        return FolderService.getFoldersByParentId(Number(params.parentId))
+    },
+
     getRootFolders: async () => FolderService.getRootFolders()
 }
